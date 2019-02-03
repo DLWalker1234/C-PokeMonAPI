b6c6ee2dc3c66c6e7c1176ac5ebf7968b152fb8e
Dustin Walker <thewalkman1234@gmail.com>
2019-02-03 16:58:50 -0600

    Initial Commit

---
+++ API/Models/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Models
+{
+
+    public class Pokemon
+    {
+        public int PokemonId { get; set; }
+
+        public string PokemonName { get; set; }
+
+        public string PokemonPrice { get; set; }
+
+    }
+}
